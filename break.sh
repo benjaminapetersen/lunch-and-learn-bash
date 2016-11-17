35d937cedd3884079f14d8ea878bae2a1f1f6b76
benjaminapetersen <admin@benjaminapetersen.me>
2016-11-17 12:13:49 -0500

    upate break

--- break.sh
+++ break.sh
@@ -12,3 +12,21 @@ do
 	fi
 	A=`expr $A + 1`
 done
+
+
+# strangely, giving break `2` in this next example
+# tells it to break out of the outer loop as well
+# (which will naturally break out of the inner)
+
+for FOO in 1 2 3
+do
+	for BAR in 0 5
+	do
+		if [ $FOO -eq 2 -a $BAR -eq 0 ] 
+		then
+			break 2	
+		else
+			echo "$FOO and $BAR"
+		fi
+	done
+done
