7f23b14185acb7cb1aac4aafdca20a37c8a0b27e
benjaminapetersen <ben@benjaminapetersen.me>
2017-11-01 15:29:39 -0400

    Update example init.sh

--- lib/init.sh
+++ lib/init.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# include this file in other bash scripts via
+# a line like this:
+# source "$(dirname "${BASH_SOURCE}")/lib/init.sh"
+
+
 set -o errexit
 set -o nounset
 set -o pipefail
+
+
+# show everything the shell is doing in case exit 1 isn't called on a fail
+set -o xtrace
